ba92dfc806e98e8525377ab35af051308da40127
krishnarao1 <krishnarao.inturi@technoidentity.com>
2015-07-14 12:49:52 +0530

    Implementaion of  NULL Marks queries

--- chapter2/2Single-Table Queries.sql
+++ chapter2/2Single-Table Queries.sql
@@ -160,3 +160,33 @@ SELECT orderid,custid,empid,orderdate from Sales.Orders where (custid=1 AND empi
 ------------------------------------------------------------------------------------------------------
 -- CASE Expressions
 ------------------------------------------------------------------------------------------------------
+/*
+A CASE expression is a scalar expression that returns a value based on conditional logic.
+*/
+SELECT productid,productname,categoryid,
+CASE categoryid
+WHEN 1 THEN 'Beverages'
+WHEN 2 THEN 'Condiments'
+WHEN 3 THEN 'Confections'
+WHEN 4 THEN 'Dairy Products'
+WHEN 5 THEN 'Grains/Cereals'
+WHEN 6 THEN 'Meat/Poultry'
+WHEN 7 THEN 'Produce'
+WHEN 8 THEN 'Seafood'
+ELSE 'Unknown Category'
+END AS categoryname
+ from Production.Products
+
+
+ SELECT orderid, custid, val,
+CASE
+WHEN val < 1000.00 THEN 'Less than 1000'
+WHEN val BETWEEN 1000.00 AND 3000.00 THEN 'Between 1000 and 3000'
+WHEN val > 3000.00 THEN 'More than 3000'
+ELSE 'Unknown'
+END AS valuecategory
+FROM Sales.OrderValues;
+
+---------------------------------------------------------------------------------
+--NULL Marks
+---------------------------------------------------------------------------------
